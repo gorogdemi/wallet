11934e43535c6cfd902dfffd333a3365dbd6b43c
Demeter Görög <demi@gorogdemi.hu>
2024-10-10 11:55:43 +0200

    Replaced Hungarian text with English.

--- Shared/Transactions/TransactionRequestValidator.cs
+++ Shared/Transactions/TransactionRequestValidator.cs
@@ -15,14 +15,14 @@ public class TransactionRequestValidator : AbstractValidator<TransactionRequest>
 
         RuleFor(request => request.BankAmount)
             .GreaterThan(0)
-            .When(request => request.CashAmount == 0)
-            .WithMessage("A tranzakció nem lehet nulla összegű.")
+            .When(request => request.CashAmount <= 0)
+            .WithMessage("The sum of the transaction must greater than zero.")
             .GreaterThanOrEqualTo(0);
 
         RuleFor(request => request.CashAmount)
             .GreaterThan(0)
-            .When(request => request.BankAmount == 0)
-            .WithMessage("A tranzakció nem lehet nulla összegű.")
+            .When(request => request.BankAmount <= 0)
+            .WithMessage("The sum of the transaction must greater than zero.")
             .GreaterThanOrEqualTo(0);
 
         RuleFor(request => request.Type)
--- WebUI/Extensions/ConverterExtensions.cs
+++ WebUI/Extensions/ConverterExtensions.cs
@@ -5,29 +5,19 @@ namespace Wallet.WebUI.Extensions;
 
 public static class ConverterExtensions
 {
-    public static string ToCategoryText(this string category) => category ?? "Nincs";
+    public static string ToCategoryText(this string category) => category ?? "None";
 
     public static string ToFormattedDate(this DateTime dateTime) => dateTime.ToString("yyyy.MM.dd", CultureInfo.InvariantCulture);
 
     public static string ToFormattedDate(this DateOnly dateOnly) => dateOnly.ToString("yyyy.MM.dd", CultureInfo.InvariantCulture);
 
-    public static string ToTransactionTypeText(this TransactionType? transactionType)
-    {
-        return transactionType switch
+    public static string ToTransactionTypeText(this TransactionType transactionType) =>
+        transactionType switch
         {
-            TransactionType.Expense => "Kiadás",
-            TransactionType.Income => "Bevétel",
+            TransactionType.Expense => "Expense",
+            TransactionType.Income => "Income",
             _ => null,
         };
-    }
 
-    public static string ToTransactionTypeText(this TransactionType transactionType)
-    {
-        return transactionType switch
-        {
-            TransactionType.Expense => "Kiadás",
-            TransactionType.Income => "Bevétel",
-            _ => null,
-        };
-    }
+    public static string ToTransactionTypeText(this TransactionType? transactionType) => ToTransactionTypeText(transactionType ?? default);
 }
